e7bdae6d69420d5e693b1d1e545971ee53aa0f08
Manjil Thapa <manjil@live.unc.edu>
2017-03-06 19:50:45 -0500

    Add files via upload

--- Import.php
+++ Import.php
@@ -1,7 +1,7 @@
 <?php
 $user = 'root';
 $password = '';
-$database = 'tapeslotting';
+$database = 'tapes';
 $tapes = $_POST["ImportTapes"];
 
 $tapeArray = explode("\n", $tapes);
@@ -10,16 +10,24 @@ $countOfArray = count($tapeArray);
 $connect = mysql_connect("localhost", $user, $password);
 @mysql_select_db($database) or ("Database not found");
 
-$query = "SELECT Location FROM `location` WHERE  Empty =1 AND Hosting = 1 LIMIT 90";
-
-$result = mysql_query($query);
 foreach (range(0, $countOfArray-1) as $number) {
+  $currentTape = $tapeArray[$number];
+  $hostingQuery = "SELECT Hosting FROM tapes WHERE Tape ='$currentTape' LIMIT 1";
+  $hostingResult = mysql_query($hostingQuery);
+  $hosting = mysql_result($hostingResult, 0);
+  $locationQuery = "SELECT Location FROM location WHERE  Empty =1 AND Hosting = '$hosting' LIMIT 1";
+  $locationResult = mysql_query($locationQuery);
+  $location = mysql_result($locationResult, 0);
+  $insertTo = "INSERT INTO tapelocation VALUES ('$currentTape', '$location','')";
+  $query4 = "UPDATE Location SET Empty ='0' WHERE Location = '$location'";
+
   echo "<u>";
   echo $tapeArray[$number];
-  echo '_____---->______';
-  echo  mysql_result($result, $number);
+  echo '_____---->_____';
+  echo  $location;
   echo "</u>";
   echo "<br>";
 
 }
+
 ?>
