5bf50553511f5da78e1b2c2666ee087a1153992e
nitoma <nick.toma@sas.com>
2017-03-07 12:22:03 -0500

    Add files via upload

---
+++ FindPrefix.php
@@ -0,0 +1,37 @@
+<?php
+$user = 'root';
+$password = '';
+$database = 'tapes';
+$prefixes = $_POST["FindPreTapes"];
+
+$prefixArray = explode(",", $prefixes);
+$countOfArray = count($prefixArray);
+
+
+$mysqli = new mysqli("localhost", $user, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+foreach (range(0, $countOfArray-1) as $number) {
+  $currentPre = $prefixArray[$number];
+  $res = $mysqli->query("SELECT Tape, Location FROM tapelocation WHERE Tape LIKE '$currentPre%'");
+  if ($res->num_rows == 0){
+	  echo "<b>"; 
+	  echo $currentPre; 
+	  echo "</b>";
+	  echo " is not in library <br>";
+  }
+  else{
+	  
+  while($row = $res->fetch_assoc()){
+   echo $row['Tape'];
+   echo " ----> ";
+   echo $row['Location'];
+   echo "<br><br>";
+  }
+
+  }
+}
+
+?>
--- Import.php
+++ Import.php
@@ -15,13 +15,21 @@ if ($mysqli->connect_errno) {
 
 foreach (range(0, $countOfArray-1) as $number) {
   $currentTape = $tapeArray[$number];
+  $res = $mysqli->query("SELECT Tape, Location FROM tapelocation WHERE Tape ='$currentTape'");
+  if ($res->num_rows > 0){
+	  echo "<b>"; 
+	  echo $currentTape; 
+	  echo "</b>";
+	  echo " is already in library <br>";
+	  die;
+  }
   $res = $mysqli->query("SELECT Hosting, Tape FROM tapes WHERE Tape ='$currentTape'");
   $row = $res->fetch_assoc();
   $hosting = $row['Hosting'];
   $locationQuery = $mysqli->query("SELECT Location FROM location WHERE  Empty =1 AND Hosting = '$hosting' LIMIT 1");
   $rowloc = $locationQuery->fetch_assoc();
   $location = $rowloc['Location'];
-  $mysqli->query("INSERT INTO tapelocation VALUES ('$currentTape', '$location','')");
+  $mysqli->query("INSERT INTO tapelocation VALUES ('$currentTape', '$location',NOW())");
   $mysqli->query("UPDATE Location SET Empty ='0' WHERE Location = '$location'");
 
   echo "<u>";
