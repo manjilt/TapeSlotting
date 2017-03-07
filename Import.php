3274786ff0364649808f8501926b60b3311723b3
nitoma <nick.toma@sas.com>
2017-03-07 10:29:27 -0500

    Add files via upload

--- Import.php
+++ Import.php
@@ -4,22 +4,28 @@ $password = '';
 $database = 'tapes';
 $tapes = $_POST["ImportTapes"];
 
-$tapeArray = explode("\n", $tapes);
-//echo $tapeArray[1];
+$tapeArray = explode(",", $tapes);
 $countOfArray = count($tapeArray);
-$connect = mysql_connect("localhost", $user, $password);
-@mysql_select_db($database) or ("Database not found");
+
+
+$mysqli = new mysqli("localhost", $user, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 
 foreach (range(0, $countOfArray-1) as $number) {
   $currentTape = $tapeArray[$number];
-  $hostingQuery = "SELECT Hosting FROM tapes WHERE Tape ='$currentTape' LIMIT 1";
-  $hostingResult = mysql_query($hostingQuery);
-  $hosting = mysql_result($hostingResult, 0);
-  $locationQuery = "SELECT Location FROM location WHERE  Empty =1 AND Hosting = '$hosting' LIMIT 1";
-  $locationResult = mysql_query($locationQuery);
-  $location = mysql_result($locationResult, 0);
-  $insertTo = "INSERT INTO tapelocation VALUES ('$currentTape', '$location','')";
-  $query4 = "UPDATE Location SET Empty ='0' WHERE Location = '$location'";
+  echo $currentTape;
+  $res = $mysqli->query("SELECT Hosting, Tape FROM tapes WHERE Tape ='$currentTape'");
+  $row = $res->fetch_assoc();
+  $hosting = $row['Hosting'];
+  echo $hosting;
+  echo "<br>";
+  $locationQuery = $mysqli->query("SELECT Location FROM location WHERE  Empty =1 AND Hosting = '$hosting' LIMIT 1");
+  $rowloc = $locationQuery->fetch_assoc();
+  $location = $rowloc['Location'];
+  $mysqli->query("INSERT INTO tapelocation VALUES ('$currentTape', '$location','')");
+  $mysqli->query("UPDATE Location SET Empty ='0' WHERE Location = '$location'");
 
   echo "<u>";
   echo $tapeArray[$number];
